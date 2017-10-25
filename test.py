f3a1178282820abc7028643ee1629ff5cae507f1
onejajae <onejajae@kookmin.ac.kr>
2017-10-25 20:14:17 +0900

    test

---
+++ test.py
@@ -0,0 +1,109 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+db = {
+    'left_forward': False,
+    'right_forward': False,
+    'left_backward': True,
+    'right_backward': True,
+    'left_motor_a': 12,
+    'left_motor_b': 11,
+    'left_motor_pwm': 35,
+    'right_motor_a': 31,
+    'right_motor_b': 33,
+    'right_motor_pwm': 37
+}
+
+
+class RaspberryCar(object):
+    def __init__(self, db):
+        self.leftMotor = LeftMotor(db)
+        self.rightMotor = RightMotor(db)
+
+    def go_forward(self, speed, time):
+        self.leftMotor.forward(speed)
+        self.rightMotor.forward(speed)
+        sleep(time)
+
+    def go_backward(self, speed, time):
+        self.leftMotor.backward(speed)
+        self.rightMotor.backward(speed)
+        sleep(time)
+
+    def stop(self):
+        self.leftMotor.stop()
+        self.rightMotor.stop()
+
+
+class Motor(object):
+    def __init__(self, pinA, pinB, pinPWM, forward, backward):
+        self.pinA = pinA
+        self.pinB = pinB
+        self.pinPWM = pinPWM
+        self.forward = forward
+        self.backward = backward
+        self.setup()
+
+    def setup(self):
+        GPIO.setup(self.pinA, GPIO.OUT)
+        GPIO.setup(self.pinB, GPIO.OUT)
+        GPIO.setup(self.PWM, GPIO.OUT)
+        self.PWM = GPIO.PWM(self.pinPWM, 100)
+        self.PWM.start(0)
+
+    def forward(self, speed):
+        if self.forward:
+            GPIO.output(self.pinA, GPIO.HIGH)
+            GPIO.output(self.pinB, GPIO.LOW)
+        else:
+            GPIO.output(self.pinA, GPIO.LOW)
+            GPIO.output(self.pinB, GPIO.HIGH)
+        GPIO.output(self.pinPWM, GPIO.HIGH)
+        self.PWM.ChangeDutyCycle(speed)
+
+    def backward(self, speed):
+        if self.backward:
+            GPIO.output(self.pinA, GPIO.LOW)
+            GPIO.output(self.pinB, GPIO.HIGH)
+        else:
+            GPIO.output(self.pinA, GPIO.HIGH)
+            GPIO.output(self.pinB, GPIO.LOW)
+        GPIO.output(self.pinPWM, GPIO.HIGH)
+        self.PWM.ChangeDutyCycle(speed)
+
+    def stop(self):
+        GPIO.output(self.pinPWM, GPIO.LOW)
+        self.PWM.ChangeDutyCycle(0)
+
+
+class LeftMotor(Motor):
+    def __init__(self, db):
+        self.pinA = db['left_motor_a']
+        self.pinB = db['left_motor_b']
+        self.pinPWM = db['left_motor_pwm']
+        self.forward = db['left_forward']
+        self.backward = db['left_backward']
+        self.setup()
+
+
+class RightMotor(Motor):
+    def __init__(self, db):
+        self.pinA = db['right_motor_a']
+        self.pinB = db['right_motor_b']
+        self.pinPWM = db['right_motor_pwm']
+        self.forward = db['right_forward']
+        self.backward = db['right_backward']
+        self.setup()
+
+
+if __name__ == "__main__":
+    myCar = RaspberryCar(db)
+    try:
+        myCar.go_forward(45, 2)
+        sleep(1)
+        myCar.go_forward(45, 2)
+        sleep(1)
+        myCar.stop()
+    except KeyboardInterrupt as e:
+        myCar.stop()
+        GPIO.cleanup()
