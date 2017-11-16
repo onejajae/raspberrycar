061db0e06bf3fa8a03b652ecedaa8eba78ee7d49
onejajae <onejajae@gmail.com>
2017-11-16 14:43:21 +0900

    configs

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+"""
 db = {
     'left_forward': True,
     'right_forward': False,
@@ -17,4 +18,25 @@ db = {
     'track_right1': 40,
     'track_right2': 32,
 
+}
+"""
+
+db = {
+    'left_forward': False,
+    'right_forward': False,
+    'left_backward': True,
+    'right_backward': True,
+    'left_motor_a': 12,
+    'left_motor_b': 11,
+    'left_motor_pwm': 35,
+    'right_motor_a': 31,
+    'right_motor_b': 33,
+    'right_motor_pwm': 37,
+    'trig': 18,
+    'echo': 16,
+    'track_left2': 29,
+    'track_left1': 32,
+    'track_center': 36,
+    'track_right1': 38,
+    'track_right2': 40,
 }
