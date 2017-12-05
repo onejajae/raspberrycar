6f8c0034fab309457d90f634736e032e6155f799
onejajae <onejajae@gmail.com>
2017-12-05 13:55:15 +0900

    test

--- mazerunner.py
+++ mazerunner.py
@@ -52,36 +52,11 @@ class MazeRunner(raspberrycar.RaspberryCar):
             self.stop()
 
     def calibrating(self):
-        self.go_forward(30, 0.5)
+        self.go_forward(30, 0.3)
         self.stop()
 
-    def mazeEscaping(self):
-        f = lambda f: f[0] + f[1] + f[2] + f[3] + f[4]
-        while True:
-            dat = self.trackSensor.getReversedStatus()
-            b_c = f(dat)
-            if b_c == 0:
-                print dat
-                raw_input('uturn')
-            elif b_c > 2 and (dat[0] or dat[4]):
-                time.sleep(2.5)
-                self.stop()
-                flag = 1 * dat[0] + 2 * dat[4]
-                print dat, 'find turn'
-                # flag 1: left, 2: right, 3: two
-                if flag == 1:
-                    print dat
-                    raw_input('left')
-                elif flag == 2:
-                    print dat
-                    raw_input('right')
-                elif flag == 3:
-                    print dat
-                    raw_input('right')
-            else:
-                self.lineTracing()
-                
-"""
+
+
     def mazeEscaping(self):
         self.differentialForward(20, 20)
         while True:
@@ -96,13 +71,19 @@ class MazeRunner(raspberrycar.RaspberryCar):
                     print 'right'
                     raw_input("re")
                 else:
-                    print 'left'
-                    raw_input("re")
+                    self.calibrating()
+                    self.stop(0.3)
+                    self.dat = self.trackSensor.getReversedStatus()
+                    if self.dat[0]:
+                        print 'left'
+                        raw_input("re")
+                    else:
+                        continue
             elif not (self.dat[0] or self.dat[1] or self.dat[2] or self.dat[3] or self.dat[4]):
                 self.stop()
                 print 'uturn'
                 raw_input("re")
-"""
+
 
 
 
